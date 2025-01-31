385ba59d1dcd61485be79feb6fe949516830d421
Pxlmastr <44294279+pxlmastrXD@users.noreply.github.com>
2025-01-31 14:26:01 -0500

    Deleted entire codebase, redoing with new objectives in mind

--- src/minibash.rs
+++ src/minibash.rs
@@ -1,22 +1,17 @@
 use std::process::Command;
 
-pub fn runbash(command: &str) -> &str {
-    let output = Command::new("bash")
-        .arg("-c")
-        .arg(command)
-        .output();
+// Tested 1/31/25
+fn runbash(x: &str) -> String {
+    let command = "bash";
+    let argument = "-c";
+    let output = Command::new(command)
+        .arg(argument)
+        .arg(x)
+        .output()
+        .expect("Failed to execute command");
 
-    if let Ok(output) = output {
-        if output.status.success() {
-            return Box::leak(String::from_utf8_lossy(&output.stdout).trim().to_string().into_boxed_str());
-        }
-    }
-    "error"
-}
-
-pub fn checkOutput(output: &str) -> bool {
-    return match output {
-        "error" => false,
-        else => true
-    }
+    let output_bytes = output.stdout;
+    let output_string = String::from_utf8_lossy(&output_bytes);
+    
+    output_string.to_string()
 }
